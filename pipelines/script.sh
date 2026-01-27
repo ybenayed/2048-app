058ef6525d91a4f37ee94f4efe19a7d09f902ed4
BEN AYED Yasmine <Yasmine.Ben_Ayed@bordeaux-inp.fr>
2026-01-27 17:16:09 +0100

    Git Merge

--- pipelines/script.sh
+++ pipelines/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Installer les dépendances du projet
 pnpm install
 
@@ -10,8 +9,22 @@ rm -rf node_modules
 # Réinstaller les dépendances
 pnpm install
 
+# Vérifier si vue-tsc est installé
+pnpm list vue-tsc
+
+# Si vue-tsc n'est pas installé, l'installer en tant que dépendance de développement
+pnpm add -D vue-tsc
+
+# Exécuter la vérification de type avec vue-tsc sans émettre de fichiers
+pnpm vue-tsc --noEmit
+
+
+
+
 # Exécuter les tests avec vitest
 echo "=========================================="
 echo "Exécution des tests unitaires avec vitest"
 echo "=========================================="
 pnpm exec vitest run
+
+
