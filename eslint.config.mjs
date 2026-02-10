ab0814378370272afa29a91341df272cec936ee8
manalelabboudi <manal@manal-Precision-3561>
2026-02-10 17:11:10 +0100

    pipeline3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
+  ignores: ['publish/**', '.output/**', 'node_modules/**'],
   files: ['**/*.vue'],
   languageOptions: {
     parserOptions: {
